f93f1df56abfd08a65fd215dcd1ff0c2ceca5802
Bruce Burgess <bruceburgess67@yahoo.com>
2016-02-22 15:38:02 +0700

    Add Navigation controller, table view

--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //Connect the storyboard table view
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set the data source and delegate to the view controller
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Set up the number of rows in the table view
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    //Set up the cells in the table view
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "Do this thing"
+        return cell
     }
 
 
